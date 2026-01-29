bc12b88dcaea8a32b5751addf7b95f0986129c75
heloris <oneqrtr@gmail.com>
2026-01-30 02:06:32 +0300

    upbttn

--- src/components/Topbar.tsx
+++ src/components/Topbar.tsx
@@ -35,16 +35,19 @@ export default function Topbar({ title }: { title: string }) {
                     <>
                         {(plan === 'plusimum' || plan === 'trial' || plan === 'freemium') && (
                             <Link href={`https://${slug}.oneqr.tr`} target="_blank" className="btn-icon-mobile" title="Siteye Git">
-                                <i className="fa-solid fa-globe"></i> <span className="hidden md:inline ml-2">Siteye Git</span>
+                                <i className="fa-solid fa-globe"></i>
+                                <span className="btn-label-desktop">Siteye Git</span>
                             </Link>
                         )}
                         {(plan === 'plusimum' || plan === 'trial' || plan === 'freemium') && (
                             <Link href={`/k/${slug}`} target="_blank" className="btn-icon-mobile purple" title="Kartviziti Gör">
-                                <i className="fa-solid fa-address-card"></i> <span className="hidden md:inline ml-2">Kartvizit</span>
+                                <i className="fa-solid fa-address-card"></i>
+                                <span className="btn-label-desktop">Kartvizit</span>
                             </Link>
                         )}
                         <Link href={`/menu/${slug}`} target="_blank" className="btn-icon-mobile outline" title="Menüyü Gör">
-                            <i className="fa-solid fa-arrow-up-right-from-square"></i> <span className="hidden md:inline ml-2">Menü</span>
+                            <i className="fa-solid fa-arrow-up-right-from-square"></i>
+                            <span className="btn-label-desktop">Menü</span>
                         </Link>
                     </>
                 )}
